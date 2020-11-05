0925b188c9a934018c3b054660c519ab63940a77
dalemav <dulim2@up.edu.ph>
2020-11-05 14:04:40 +0800

    add test_adder with fixtures

--- 07_Fixtures_Activity/my_library/tests/test_adder.py
+++ 07_Fixtures_Activity/my_library/tests/test_adder.py
@@ -2,5 +2,23 @@
 import pytest
 from .. import my_module
 
+@pytest.fixture(scope="session")
+def adder_fixture():
+    """Adder fixture function"""
+    fixture_object = my_module.SingleObject(1)
+    return fixture_object
 
+def test_add_more_5(adder_fixture):
+    """Test add_more 5"""
+    result = adder_fixture.add_more(5)
+    assert result == 6
 
+def test_add_more_7(adder_fixture):
+    """Test add_more 7"""
+    result = adder_fixture.add_more(7)
+    assert result == 8
+
+def test_add_more_10(adder_fixture):
+    """Test add_more 10"""
+    result = adder_fixture.add_more(10)
+    assert result == 11
