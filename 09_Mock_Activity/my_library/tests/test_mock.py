0f991776902e207e28ab38636698b05b047045e1
dalemav <dulim2@up.edu.ph>
2020-11-09 16:51:38 +0800

    fill in test_mock.py

--- 09_Mock_Activity/my_library/tests/test_mock.py
+++ 09_Mock_Activity/my_library/tests/test_mock.py
@@ -3,10 +3,12 @@ from .. import my_module
 
 
 def test_big():
-
+    series = Mock()
+    series.sum.return_value = 1001
     assert my_module.is_sum_of_series_big(series) is True
 
 
 def test_not_big():
-
+    series = Mock()
+    series.sum.return_value = 999
     assert my_module.is_sum_of_series_big(series) is False
